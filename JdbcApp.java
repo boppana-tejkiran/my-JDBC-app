7874a75ad1b75154dccdd0503abd167c9d771261
tejkiran <kirantejboppana@gmail.com>
2022-12-28 15:24:51 +0530

    Create TABLE in database using JDBC

--- JdbcApp.java
+++ JdbcApp.java
@@ -4,10 +4,11 @@ import java.sql.*;
 class JdbcApp{
 	public static void main(String args[]){
 		try{ //checked exception
-			//load the driver
+			
+			/* 1. load the driver*/
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
-			//create connection
+			/* 2. create connection */
 			String url = "jdbc:mysql://localhost:3306/youtube";
 			String username = "root";
 			String password = "";
@@ -19,6 +20,16 @@ class JdbcApp{
 			else{
 				System.out.println("Connection Created...");
 			}
+			
+			/* 3. Create Query */
+			String q = "create table table1(tId int(20) primary key auto_increment, tName varchar(200) not null, tCity varchar(400))";
+			
+			/* 4. Create a statement*/
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate(q);
+			System.out.println("Table created in Database..");
+			
 		}catch(Exception e)
 		{
 			e.printStackTrace();
